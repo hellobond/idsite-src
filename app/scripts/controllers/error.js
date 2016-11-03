0a04790b057d5d15f3aa47fc3e29262877299e48
singuista <tensai213@gmail.com>
2016-11-03 11:05:13 -0400

    error and verify screens

--- app/scripts/controllers/error.js
+++ app/scripts/controllers/error.js
@@ -1,10 +1,13 @@
 'use strict';
 
 angular.module('stormpathIdpApp')
-  .controller('ErrorCtrl', function ($scope,Stormpath) {
-    $scope.errors = Stormpath.errors;
-    $scope.inError = false;
-    $scope.$watchCollection('errors',function(){
-      $scope.inError = $scope.errors.length > 0;
-    });
-  });
+.controller('ErrorCtrl', function ($scope,Stormpath) {
+	$scope.errors = Stormpath.errors;
+	$scope.inError = false;
+
+	$scope.$watchCollection('errors',function() {
+		//console.log('Stormpath.errors = ', Stormpath.errors);
+		$scope.inError = $scope.errors.length > 0;
+      	//$scope.inError = true;
+  	});
+});
